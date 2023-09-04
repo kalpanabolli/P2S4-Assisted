0be8409e66a63059896a3db96359bcda3b85419c
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-09-04 20:21:33 +0530

    first commit

---
+++ S4-1.20/S4-1.20/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phase3Section2._18.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string WhichClass { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+
+
+    }
+}
